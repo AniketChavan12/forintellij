42bbb1e02c43450703d199814f11ff0767e94320
Aniketcha5 <caniket795@gmail.com>
2020-08-01 17:26:03 +0530

    second comiit

---
+++ src/main/java/Theopenclass.java
@@ -0,0 +1,42 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Theopenclass {
+
+
+    public static void startup() throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.guru99.com/");
+        Thread.sleep(5000);
+
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        driver.get("https://in.yahoo.com/");
+
+        driver.navigate().back();
+        driver.switchTo().newWindow(WindowType.TAB);
+        Thread.sleep(2000);
+        driver.get("https://mvnrepository.com/");
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ src/test/java/Testclass.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class Testclass {
+
+
+    @Test
+    public static void launch() throws InterruptedException {
+
+        Theopenclass.startup();
+    }
+
+
+
+
+}
